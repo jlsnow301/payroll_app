8c29ddae8f72a5f7a376fec9cf78989516c405cc
jlsnow301 <jlsnow301@pm.me>
2025-08-30 01:25:58 -0700

    initial commit

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,71 @@
+use std::{path::Path, sync::Mutex};
+
+use tauri::{Builder, Manager, State};
+
+use crate::responses::ApiResponse;
+
+mod responses;
+
+#[derive(Default)]
+struct AppState {
+    caterease: String,
+    intuit: String,
+}
+
+#[tauri::command]
+fn caterease_input(file_path: String, state: State<'_, Mutex<AppState>>) -> ApiResponse<String> {
+    let path = Path::new(file_path.as_str());
+
+    if !path.exists() {
+        return ApiResponse::error("FILE_NOT_FOUND", "File doesn't exist");
+    }
+
+    let mut state = state.lock().unwrap();
+
+    state.caterease = file_path;
+
+    ApiResponse::success("Success".to_string())
+}
+
+#[tauri::command]
+fn intuit_input(file_path: String, state: State<'_, Mutex<AppState>>) -> ApiResponse<String> {
+    let path = Path::new(file_path.as_str());
+
+    if !path.exists() {
+        return ApiResponse::error("FILE_NOT_FOUND", "File doesn't exist");
+    }
+
+    let mut state = state.lock().unwrap();
+
+    state.intuit = file_path;
+
+    ApiResponse::success("Success".to_string())
+}
+
+#[tauri::command]
+fn submit(state: State<'_, Mutex<AppState>>) -> ApiResponse<String> {
+    let state = state.lock().unwrap();
+
+    if state.caterease.is_empty() || state.intuit.is_empty() {
+        return ApiResponse::error("REQUIRED_FIELD_MISSING", "Both documents must be linked");
+    }
+
+    ApiResponse::success("Uploaded".to_string())
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    Builder::default()
+        .setup(|app| {
+            app.manage(Mutex::new(AppState::default()));
+            Ok(())
+        })
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![
+            caterease_input,
+            intuit_input,
+            submit
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src-tauri/src/responses.rs
@@ -0,0 +1,35 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ApiResponse<T> {
+    pub success: bool,
+    pub data: Option<T>,
+    pub error: Option<ApiError>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ApiError {
+    pub code: String,
+    pub message: String,
+}
+
+impl<T> ApiResponse<T> {
+    pub fn success(data: T) -> Self {
+        Self {
+            success: true,
+            data: Some(data),
+            error: None,
+        }
+    }
+
+    pub fn error(code: &str, message: &str) -> Self {
+        Self {
+            success: false,
+            data: None,
+            error: Some(ApiError {
+                code: code.to_string(),
+                message: message.to_string(),
+            }),
+        }
+    }
+}
