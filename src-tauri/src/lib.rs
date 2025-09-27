0fd284478b3b82c7d6687aeb1e753b0cd77d83d8
jlsnow301 <jlsnow301@pm.me>
2025-09-27 13:27:02 -0700

    test

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -25,6 +25,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             caterease_input,
             get_headers,
