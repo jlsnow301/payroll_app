2f7b396b141f609c2d4fda061c03db9af9667d74
jerm <jlsnow301@pm.me>
2025-09-27 13:12:25 -0700

    js updater

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { relaunch } from "@tauri-apps/plugin-process";
+import { check } from "@tauri-apps/plugin-updater";
+import { useEffect } from "react";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { Page, useSimpleRouter } from "./hooks.ts";
 import { HomePage } from "./pages/home/index.tsx";
@@ -8,6 +11,10 @@ const queryClient = new QueryClient();
 
 export function App() {
   const [page] = useSimpleRouter();
+
+  useEffect(() => {
+    checkUpdates();
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -19,4 +26,43 @@ export function App() {
       </ThemeProvider>
     </QueryClientProvider>
   );
+}
+
+/**
+ * Straight from the docs
+ * @see https://v2.tauri.app/plugin/updater/#checking-for-updates
+ */
+async function checkUpdates() {
+  const update = await check();
+  if (!update) {
+    console.log("no update available");
+    return;
+  }
+
+  console.log(
+    `found update ${update.version} from ${update.date} with notes ${update.body}`,
+  );
+  let downloaded = 0;
+  let contentLength = 0;
+
+  await update.downloadAndInstall((event) => {
+    switch (event.event) {
+      case "Started":
+        contentLength = event.data.contentLength || 0;
+        console.log(
+          `started downloading ${event.data.contentLength} bytes`,
+        );
+        break;
+      case "Progress":
+        downloaded += event.data.chunkLength;
+        console.log(`downloaded ${downloaded} from ${contentLength}`);
+        break;
+      case "Finished":
+        console.log("download finished");
+        break;
+    }
+  });
+
+  console.log("update installed");
+  await relaunch();
 }
